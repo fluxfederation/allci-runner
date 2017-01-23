f8495e8180fd05b584b7be5d5a69751068362089
Will Bryant <will.bryant@gmail.com>
2017-01-23 22:17:42 +1300

    put the containers into a network so they can reference each other by name, and also use this to find containers to remove

--- runner.rb
+++ runner.rb
@@ -37,12 +37,21 @@ runner_name = ENV["RUNNER_NAME"] || Socket.gethostname
 client = ServiceClient.new(ENV["CI_SERVICE_URL"], "runner_name": runner_name)
 
 buildroot = "tmp/build"
+pod_name = "allci-runner"
 
 poll_frequency = ENV["CI_POLL_FREQUENCY"].to_i
 poll_frequency = 5 if poll_frequency.zero?
 
 failed_poll_frequency = ENV["CI_FAILED_POLL_FREQUENCY"].to_i
 failed_poll_frequency = poll_frequency if failed_poll_frequency.zero?
+
+def create_pod(pod_name)
+  system "docker network create --driver bridge #{pod_name}"
+end
+
+def remove_pod(pod_name)
+  system "docker rm -f $(docker ps --quiet --filter network=#{pod_name}); docker network rm #{pod_name}"
+end
 
 def build_images(task, buildroot)
   workdir = "#{buildroot}/workdir"
@@ -81,8 +90,11 @@ def pull_images(task)
   true
 end
 
-def run_options(container_name, container_details)
+def run_options(pod_name, container_name, container_details)
   args = ["--rm", "-a", "STDOUT", "-a", "STDERR"]
+
+  args << "--network"
+  args << pod_name
 
   args << "--hostname"
   args << (container_details["hostname"] || container_name)
@@ -103,10 +115,11 @@ def run_options(container_name, container_details)
   args
 end
 
-def run_task(task)
+def run_task(pod_name, task)
   # fork and run each container
+  create_pod(pod_name)
   children = task["components"].collect do |container_name, container_details|
-    fork { Kernel.exec("docker", "run", *run_options(container_name, container_details)) }
+    fork { Kernel.exec("docker", "run", *run_options(pod_name, container_name, container_details)) }
   end
 
   # if we fail to spawn a child process, we've already failed
@@ -120,7 +133,7 @@ def run_task(task)
     children.delete(exited_child)
 
     # then tell all the others to terminate
-    children.each { |child| Process.kill('TERM', child) }
+    remove_pod(pod_name)
   end
 
   output = "TODO: some container failed :(" unless success
@@ -137,9 +150,10 @@ loop do
     if task["stage"] == "bootstrap"
       success, output = build_images(task, buildroot)
       success, output = push_images(task) if success
+      success, output = run_task(pod_name, task) if success
     else
       success, output = pull_images(task)
-      success, output = run_task(task) if success
+      success, output = run_task(pod_name, task) if success
     end
 
     if success
